d7ed3a0240d4f0885d6e48dcae399c0603f04ee5
Vianney418 <123280157+Vianney418@users.noreply.github.com>
2024-10-22 01:12:16 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,7 +20,7 @@ pipeline {
             steps { 
                 script{
                     //sh 'npm install newman@5.2.3 newman-reporter-htmlextra@1.19.0'
-                    sh 'npx newman run tests\API_Prueba.postman_collection.json -e enviroment\testpostman.postman_environment.json --reporters cli,junit,htmlextra --reporter-junit-export results/TEST-postman-results.xml --reporter-htmlextra-export results/TEST-postman-results.html --suppress-exit-code'
+                    sh 'npx newman run tests/API_Prueba.postman_collection.json -e enviroment/testpostman.postman_environment.json --reporters cli,junit,htmlextra --reporter-junit-export results/TEST-postman-results.xml --reporter-htmlextra-export results/TEST-postman-results.html --suppress-exit-code'
                       
                 }
             }
